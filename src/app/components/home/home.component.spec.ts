af4f579c539c7d2c761fbe9dbc900634a1906ee4
Indika Sirimanna <indikasampath@gmail.com>
2023-02-25 08:09:23 +0530

    Commit8

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -1,3 +1,8 @@
+/* This is a test code written using the Jasmine testing framework for the HomeComponent in Angular. 
+The code imports the TestBed, HomeComponent and ComponentFixture from the @angular/core/testing library. 
+The code then uses TestBed.configureTestingModule to configure the HomeComponent, and creates the component using TestBed.createComponent. 
+Finally, the code uses the expect() method to check if the component is truthy. 
+25/02/2023 Indika Sirimanna */
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
--- src/app/models/ToDoList.ts
+++ src/app/models/ToDoList.ts
@@ -1,3 +1,9 @@
+/* This code defines an interface named ToDoList with two properties:
+    id of type number: This property represents the unique identifier for each ToDo item.
+    item of type string: This property represents the text of the ToDo item.
+The ToDoList interface can be used to define the shape of objects that will be used as ToDo items in the application. For example, a list of ToDo items can be defined as an array of objects of type ToDoList, where each object represents a single item in the list and contains the id and item properties.
+Using interfaces is a good practice in TypeScript as it helps in making the code more maintainable, easier to read and understand, and reduces the chances of introducing bugs by enforcing a consistent shape of data throughout the application. 
+25/02/2023 Indika Sirimanna*/
 export interface ToDoList {
     id: number;
     item: string;  
